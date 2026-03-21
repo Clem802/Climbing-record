9f76fea193861e023c8efa679b5cfbe7ea5d0ded
Clem Day <clem@climbing.app>
2026-03-21 17:50:20 +0000

    feat: client scaffold with Vite + React + Tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: { DEFAULT: '#f97316', dark: '#ea580c' },
+      },
+    },
+  },
+  plugins: [],
+};
